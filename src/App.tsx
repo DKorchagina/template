0c67123e31aea8bf04bc650607866aed6ad1b714
DKorchagina <dasha1972001@yandex.ru>
2022-12-15 22:01:36 +0300

    Transition to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Header } from './components/Header/Header';
+import { Main } from './components/Main/Main';
+import { MainSearch } from './components/Main/MainSearch';
+import { Footer } from './components/Footer/Footer';
+import './styles.css';
 
 export function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/search' element={<MainSearch/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
  }
 
